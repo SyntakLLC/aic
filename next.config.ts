30952ab3c669cda27b72dc83674e8968b6efbc41
Amaan Ali <amaanali2002@hotmail.com>
2025-01-25 22:25:48 -0500

    test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: process.env.NODE_ENV === "production" ? "/aic" : "",
+    basePath: "/aic",
 };
 
 export default nextConfig;
