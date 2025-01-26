1fea926fe85243fde844c18c19fb1a6b3c78eec3
Amaan Ali <amaanali2002@hotmail.com>
2025-01-25 21:02:07 -0500

    green

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,9 @@ import clsx from 'clsx'
 
 const styles = {
   primary:
-    'rounded-full bg-sky-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-sky-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-300/50 active:bg-sky-500',
+    'rounded-full bg-emerald-300 py-2 px-4 text-sm font-semibold text-slate-900 hover:bg-emerald-200 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-300/50 active:bg-emerald-500',
   secondary:
-    'rounded-full bg-slate-800 py-2 px-4 text-sm font-medium text-white hover:bg-slate-700 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400',
+    'rounded-full bg-[#0A1110] py-2 px-4 text-sm font-medium text-white hover:bg-[#141c1b] focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white/50 active:text-slate-400',
 }
 
 export function Button({ variant = 'primary', className, href, ...props }) {
